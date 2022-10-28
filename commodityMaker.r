5986d31edb0d45ae0f8b368a356b62b9f4cb9450
Matthew McCullough <mcculloughmatthew5@gmail.com>
2022-10-27 21:45:27 -0700

    Add skim of main data set.

--- commodityMaker.r
+++ commodityMaker.r
@@ -1,11 +1,26 @@
 library(skimr)
 library(tidyverse)
 
-comm1 = read_csv("uranium.csv")
+comm1 = read_csv("uranium_and_metals.csv")
 clean1 = comm1 %>%
   select(Year, Month, Price_uran) %>%
-  filter(year >=2000)
+  filter(Year >=2000)
+write_csv(clean1, "uranium.csv")
 
-print(clean1)
 comm2 = read_csv("coal-prices.csv")
+clean2 = comm2 %>%
+  select(Year,Price_coal)
+write_csv(clean2, "coal.csv")
+
 comm3 = read_csv("commodity 2000-2022.csv")
+
+clean3 = comm3 %>%
+  select(Symbol,Date,Close) %>%
+  filter(Symbol == "Brent Oil" | Symbol == "Natural Gas") %>%
+  pivot_wider(names_from = Symbol, values_from = Close) %>%
+  rename(Price_oil = "Brent Oil", Price_natural_gas = "Natural Gas")
+print(clean3)
+write_csv(clean3, "OilandGas.csv")
+  
+comm4 = read_csv("organised_Gen.csv")
+skim(comm4)
